53fb0d42816f804564e8a60f23f46ecd26f5efe4
LightChimera <artemilin347@gmail.com>
2024-02-10 19:50:02 +0300

    Добавить readme

--- get-my-manga.sh
+++ get-my-manga.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
+
 base_url="https://api.mangadex.org/"
 base_folder="./manga-get-result"
+result_file="result.json"
 
-if [ -d $base_folder ]; then
-     rm -rf $base_folder
-fi
- 
-mkdir manga-get-result && cd $_
-touch headers.txt result.json
+rm -rf $base_folder
+mkdir $base_folder && cd $_
 
-echo -e "Введите название манги для поиска:"
+echo -e "Введите название манги для поиска (на английском):"
 read manga_title
 
 echo -e "Осуществляю поиск по названию $manga_title"
 
-curl -X GET "${base_url}/manga?title=\"${manga_title}\"" \
+echo $(curl -X GET "${base_url}/manga?title=${manga_title}" \
      -H 'accept: application/json' \
      -D 'headers.txt' \
-     -o 'result.json'
+     -o $result_file)
 
 
-jq . result.json
+jq . $result_file
